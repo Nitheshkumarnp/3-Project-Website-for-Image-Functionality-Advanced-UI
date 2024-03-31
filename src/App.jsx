6f0d45e16da5efb6620ca415754fd5bcfe850095
Nitheshkumar <63224346+Nitheshkumarnp@users.noreply.github.com>
2024-03-31 09:16:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,60 @@
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [images, setImages] = useState([]);
+  const [file, setFile] = useState(null);
+  const [fileList, setFileList] = useState([]);
+  const [message, setMessage] = useState(null);
 
-  const handleUpload = async (e) => {
-    const file = e.target.files[0];
+  const handleFile = async (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const handleUpload = async () => {
     const formData = new FormData();
     formData.append('image', file);
 
     try {
-      const response = await fetch('https://service-image-upload-retrieval.onrender.com/upload', {
+      const response = await fetch('https://image-advanced-functionality-services.onrender.com/upload', {
         method: 'POST',
         body: formData,
       });
       const data = await response.json();
-      console.log(data.message); // Log success message
+      setMessage(data.message);
     } catch (error) {
       console.error('Upload error:', error);
     }
   };
 
-  const handleGetImages = async () => {
+  const handleGetAllList = async () => {
     try {
-      const response = await fetch('https://service-image-upload-retrieval.onrender.com/images?id=17');
-      if (!response.ok) {
-          throw new Error('Failed to fetch image');
-      }
-      const blob = await response.blob();
-      setImages(blob);
+      const response = await fetch('https://image-advanced-functionality-services.onrender.com/list');
+      const data = await response.json();
+      setFileList(data.fileList);
     } catch (error) {
       console.error('Get images error:', error);
     }
   };
 
-  // useEffect(() => {
-  //   handleGetImages(); // Fetch images on initial load
-  // }, []);
+  const clearAll = async () => {
+    setFile(null);
+    setFileList([]);
+    setMessage(null);
+  };
+
+  useEffect(() => {
+    clearAll();
+  }, []);
 
   return (
     <div>
-      <input type="file" onChange={handleUpload} />
-      <button onClick={handleGetImages}>Get Images</button>
-      {images.map((image, index) => (
-        <img key={index} src={`data:image/png;base64,${image.image_data}`} alt={`Image ${index}`} />
-      ))}
+      <input type="file" onChange={handleFile} />
+      <button onClick={handleUpload}>Upload</button>
+      <button onClick={handleGetAllList}>Get All File List</button>
+      <button onClick={clearAll}>Clear</button>
+      {message ? <p>{message}</p> : null}
+      {fileList.length > 0 ? fileList.map((file) => (
+        <p>{file}</p>
+      )) : null}
     </div>
   );
 }
